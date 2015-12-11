90814fa62bf3441892c8c43a1458268ee3fb725d
Noel Welsh <noel@untyped.com>
2015-12-11 12:48:18 +0000

    Simplify authentication pushing failure into the interpreter

--- free/src/main/scala/free/Auth.scala
+++ free/src/main/scala/free/Auth.scala
@@ -2,9 +2,12 @@ package free
 
 import cats.free.Free
 import cats.{~>,Id}
+import cats.data.Xor
+import cats.syntax.xor._
 
 object Auth {
   type UserId = Int
+  type Result[A] = String Xor A
 
   final case class Credentials(secretKey: String)
   sealed trait Action
@@ -12,36 +15,37 @@ object Auth {
   final case object Write extends Action
 
   sealed trait Auth[A]
-  final case class Authenticate(user: UserId, password: String) extends Auth[Option[Credentials]]
-  final case class Authorize(credentials: Credentials, action: Action) extends Auth[Boolean]
+  final case class Authenticate(user: UserId, password: String) extends Auth[Credentials]
+  final case class Authorize(credentials: Credentials, action: Action) extends Auth[Unit]
 
   object Auth {
-    def authenticate(userId: UserId, password: String): Free[Auth, Option[Credentials]] =
+    def authenticate(userId: UserId, password: String): Free[Auth, Credentials] =
       Free.liftF(Authenticate(userId, password))
 
-    def authorize(credentials: Credentials, action: Action): Free[Auth, Boolean] =
+    def authorize(credentials: Credentials, action: Action): Free[Auth, Unit] =
       Free.liftF(Authorize(credentials, action))
   }
 
-  object IdInterpreter extends (Auth ~> Id) {
+  object XorInterpreter extends (Auth ~> Result) {
     import Id._
 
-    def apply[A](in: Auth[A]): Id[A] =
+    def apply[A](in: Auth[A]): Result[A] =
       in match {
         case Authenticate(userId, password) =>
           println(s"Authenticating $userId")
           userId match {
-            case 1 => Some(Credentials("AgnesSecret"))
-            case 2 => Some(Credentials("BrianSecret"))
-            case _ => None
+            case 1 => Credentials("AgnesSecret").right
+            case 2 => Credentials("BrianSecret").right
+            case _ => s"User $userId did not successfully authenticate".left
           }
 
         case Authorize(credentials, action) =>
           println(s"Authorizing $credentials for $action")
           (credentials, action) match {
-            case (Credentials("AgnesSecret"), _)    => true
-            case (Credentials("BrianSecret"), Read) => true
-            case _                                  => false
+            case (Credentials("AgnesSecret"), _)    => ().right
+            case (Credentials("BrianSecret"), Read) => ().right
+            case _ =>
+              s"$credentials is not authorized to perform $action".left
           }
       }
   }
